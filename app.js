9a50d6a65cb71bdfb3a66fe5733e94bfd306b05e
umairsolangi <51870556+umairsolangi@users.noreply.github.com>
2021-01-02 09:22:33 +0500

    JS Assignment 1 20

---
+++ app.js
@@ -0,0 +1,540 @@
+// Chapter No 1 Question No 01
+// alert("Well Come In Our Website")
+
+
+// Chapter No 1 Question No 02
+// alert("Error!! Plz Enter a Valid Password")
+
+
+// Chapter No 1 Question No 03
+// alert("Welcome To JS Land\nHappy Coding" )
+
+
+// Chapter No 1 Question No 04
+// alert("Welcome To JS Land")
+// alert("Happy Coding")
+
+
+// Chapter No 1 Question No 05
+// console.log("Helow, I can run JS Throug Window Console")
+
+
+
+
+// Chapter No 2 Question No 01
+// var username;
+
+
+// Chapter No 2 Question No 02
+// var myname= "Umair_Solangi"
+
+
+// Chapter No 2 Question No 03
+// var title = "Hellow World"
+// alert(title)
+
+
+// Chapter No 2 Question No 04
+// var name = "Umair_Solangi"
+// var age = "20 Year"
+// var edu = "Cerificed Mobile Application Development"
+//  alert(name)
+//  alert(age)
+//  alert(edu)
+
+
+
+// Chapter No 2 Question No 05
+// var pizza = "Pizza\nPizz\nPiz\nPi\nP"
+// alert(pizza)
+
+
+
+// Chapter No 2 Question No 06
+// var email = "solangitv@gmail.com"
+// alert(email)
+
+
+
+// Chapter No 2 Question No 07
+// var book = "A Smarter Way To Learn JavaScript"
+// alert(book)
+
+
+
+
+// Chapter No 2 Question No 08
+// document.write("Hah, I can write HTML Content Using JavaScript")
+
+
+
+
+// Chapter No 2 Question No 09
+// alert("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”")
+
+
+
+
+
+// Chapter No 3 Question No 01
+// var age = "I am 20 Year Old"
+// alert(age)
+
+
+
+// Chapter No 3 Question No 03
+// var birthyear = "My Birth Year is 2000"
+// document.write(birthyear)
+
+
+
+// Chapter No 3 Question No 04
+// var visitor_name = "Umair_Solangi"
+// var product_title = "BizzCoin"
+// var quantity = "1000"
+// document.write(visitor_name, "Ordared", product_title, "& the Quantity is", quantity)
+
+
+
+
+// Chapter No 5 Question No 01
+// num1 = 3; 
+// num2 = 5;
+// total = num1+num2;
+// document.write("Sum of ", num1, "and", num2, "is", total)
+
+
+
+
+// Chapter No 5 Question No 02
+//  num1 = 13; 
+//  num2 = 5;
+//  total = num1-num2;
+//  document.write("Subtract of ", num1, "and", num2, "is", total, "<br>")
+
+//  num1 = 13; 
+//  num2 = 5;
+//  total = num1*num2;
+//  document.write("multiplication of ", num1, "and", num2, "is", total, "<br>")
+
+//  num1 = 13; 
+//  num2 = 5;
+//  total = num1/num2;
+//  document.write("Division of ", num1, "and", num2, "is", total)
+
+
+
+
+
+// Chapter No 5 Question No 04
+// var ticket = 1100;
+// var total = ticket*5;
+// document.write("Total Cost Of 5 Ticket is", total)
+
+
+
+// Chapter No 5 Question No 05
+// var t ;
+// for (var i=1; i<=10; i++) {
+//     document.write(i*2,"<br>")
+// }
+
+
+
+// Chapter No 5 Question No 06
+// var pirce_item_1 = 1200;
+// var pirce_item_2 = 1700;
+// var order_quantity_item_1 = 10;
+// var order_quantity_item_2 = 20;
+// var shipping_charges = 400;
+// var subTotal = pirce_item_1*order_quantity_item_1+shipping_charges;
+// var subTotal2 = pirce_item_2*order_quantity_item_2;
+// var total = subTotal+subTotal2;
+
+
+// document.write("Price Of Item 1 is", pirce_item_1, "<br>")
+// document.write("Quantity Of Item 1 is", order_quantity_item_1, "<br>")
+// document.write("Price Of Item 2 is", pirce_item_2, "<br>")
+// document.write("Quantity Of Item 2 is", order_quantity_item_2, "<br>")
+// document.write("ShippingCharges is",shipping_charges,"<br>","<br>")
+// document.write("Total",total )
+
+
+
+
+// Chapter No 5 Question No 07
+// var obtain = 1800;
+// var total = 2000;
+// var percentage = obtain/total*100;
+// document.write("Obtain Marks", obtain, "<br>")
+// document.write("Total Marks", total,"<br>")
+// document.write("Percentage", percentage)
+
+
+
+// Chapter No 5 Question No 11
+// var current_year = 2020;
+// var birth_year = 2000;
+// var age = current_year-birth_year;
+// document.write("Current Year Is", current_year,"<br>")
+// document.write("Your Birth Year Is", birth_year,"<br>")
+// document.write("Your Age is", age)
+
+
+
+
+
+// Chapter No 06 To 09 Question No 03
+// var name = prompt("Enter Your Good Name")
+// alert("Good Morning Mr"  + name)
+
+
+
+// Chapter No 06 To 09 Question No 05
+
+// var Table = prompt("Enter Number Which Table You Want To Print")
+
+
+//      for (i=1; i<=10; i++){
+//         document.write(i*2,"<br>")
+
+//   }
+
+
+
+
+
+// Chapter No 9 To 11 Question No 01
+// var city = prompt("Enter City")
+// alert("Welcome To" + " " + city + " " + "City")
+
+
+
+
+// Chapter No 9 To 11 Question No 02
+// var input  = prompt("Enter Your Gender")
+// if (input = "male"){
+//     alert("Good Morning Mr")
+// }
+// else if ( input = "female"){
+//         alert("Good Morning Miss")
+//         }
+// else {
+//     alert("Your are not Human")
+// }
+
+
+
+// Chapter No 9 To 11 Question No 04
+// var feul = prompt("Enter Your Remain Feul")
+// if (feul<=0.25){
+//     alert("Plz ReFill the Feul in Your Car")
+// }
+// else {
+//     alert("You can Servive")
+// }
+
+
+
+// Chapter No 9 To 11 Question No 06
+// var eng = +prompt("Enter Your English Marks")
+// var computer = +prompt("Enter Your English compter")
+// var syclogy = +prompt("Enter Your English syclogy")
+// var total_marks = 300;
+// var obtain = eng+computer+syclogy;
+// var percentage = eng+computer+syclogy/300*100;
+
+// document.write("Total Marks:", total_marks, "<br>")
+// document.write("Your Obtain Marks:",obtain , "<br>" )
+// document.write("Your Percentage:", percentage)
+
+
+
+
+// Chapter No 9 To 11 Question No 07
+// var guess = prompt("Guess the Number") 
+// var snum = "4";
+// if (guess === snum){
+//     alert("Bingo! Correct Answer")
+// }
+// else {
+//     alert("Wrong Answer Try Again")
+// }
+
+
+
+
+// Chapter No 9 To 11 Question No 10
+// var weather = prompt("Enter Your City Weather")
+// if (weather <= 10){
+//     alert("“OMG! Today’s weather is so Cool.")
+// }
+// else if ( weather <=20){
+//     alert("Today’s Weather is cool.")
+// }
+// else if ( weather <=30){
+//     alert("The Weather today is Normal.")
+// }
+// else if ( weather <=40){
+//     alert("“It is too hot outside.")
+// }
+// else {
+//     alert("May Allah Give You Protect")
+// }
+
+
+
+
+
+    
+// Chapter No 9 To 11 Question No 11
+// var num1 = +prompt("Enter First Number")
+// var num2 = +prompt("Enter Second Number")
+// var operator = prompt("Enter Operator")
+// var add  = num1+num2;
+// var sub  = num1-num2;
+// var multi  = num1*num2;
+// var divid  = num1/num2;
+
+// if (operator=== "+"){
+//     document.write(add)
+// }
+// else if ( operator=== "-"){
+//     document.write(sub)
+// }
+// else if ( operator=== "*"){
+//     document.write(sub)
+// }
+// else if ( operator=== "-"){
+//     document.write(sub)
+// }
+
+
+
+
+
+
+    
+
+// Chapter No 12 To 13 Question No 2
+// var num1 = prompt("Enter First Number")
+// var num2 = prompt("Enter Second Number")
+// if (num1>=num2){
+//     alert(num1)
+// }
+// else if (num2>=num1){
+//     alert(num2)
+// }
+// else {
+//     alert("Both are equal")
+// }
+
+
+
+// Chapter No 12 To 13 Question No 3
+// var num = prompt("Enter First Number")
+//  if (num<=1000000){
+//      alert("Your Number Is Positve")
+//  }
+//  else if (num== -1000000){
+//      alert("Your Number Is Negative")
+//  }
+//  else {
+//      alert("Your Number Is Zero")
+//  }
+
+
+
+// Chapter No 12 To 13 Question No 5
+// var enterPass = prompt("Enter Your Password")
+// var pass = "Umair_Solangi"
+// if (enterPass===pass){
+//     alert("Welcome To Our Software Network")
+// }
+// else{
+//     alert("Plz Enter Valid Password")
+// }
+
+
+
+// Chapter No 12 To 13 Question No 6
+// var hour = 13;
+// if (hour <18) {
+// greeting = alert("Good day");
+// }
+// else {
+// greeting = alert("Good evening");
+// }
+
+
+
+
+// Chapter No 12 To 13 Question No 7
+// var time = prompt("Enter Time In 24 Hour Formet")
+// if (time==13){
+//     alert("You Entred At 1:00 PM")
+// }
+// else if (time==14){
+//     alert("You Entred At 2:00 PM ")
+// }
+// else if (time==15){
+//     alert("You Entred At 3:00 PM")
+// }
+//  else if  (time==16){
+//     alert("You Entred At 4:00 PM")
+// }
+//  else if  (time==17){
+//     alert("You Entred At 5:00 PM")
+// }
+//  else if  (time==18){
+//     alert("You Entred At 6:00 PM")
+// }
+//  else if  (time==19){
+//     alert("You Entred At 7:00 PM")
+// }
+//  else if  (time==20){
+//     alert("You Entred At 8:00 PM ")
+// }
+//  else if  (time==21){
+//     alert("You Entred At 9:00 PM")
+// }
+//  else if  (time==22){
+//     alert("You Entred At 10:00 PM")
+// }
+//  else if  (time==23){
+//     alert("You Entred At 11:00 PM")
+// }
+//  else if  (time=24){
+//     alert("You Entred At 12:00 PM")
+// }
+// else{
+//     alert("You Entred At Wrong Time")
+// }
+
+
+
+
+
+// Chapter No 14 To 16 Question No 1
+// var student_name;
+
+
+
+// Chapter No 14 To 16 Question No 2
+// var degree = ["Matric", "FSc", "B Tech", "Bachlor", "Master",  "<br>"]
+// document.write(degree )
+
+
+
+
+
+// Chapter No 17 To 20 Question No 1
+// var (i=0; i<=100;i++){
+
+// }
+
+// Chapter No 17 To 20 Question No 3
+// for (i=0; i<=10; i++){
+//     document.write(i,"<br>")
+// }
+
+
+
+// Chapter No 17 To 20 Question No 4
+// var table = prompt("Enter Which Multiplication Table You Want To Print")
+// function table2(){
+//     for (i=1; i<=10; i++){
+//         document.write(i*2,"<br>")
+//     }
+// }
+// function table3(){
+//     for (i=1; i<=10; i++){
+//         document.write(i*3,"<br>")
+//     }
+// }
+// function table4(){
+//     for (i=1; i<=10; i++){
+//         document.write(i*4,"<br>")
+//     }
+// }
+// function table5(){
+//     for (i=1; i<=10; i++){
+//         document.write(i*5,"<br>")
+//     }
+// }
+// function table6(){
+//     for (i=1; i<=10; i++){
+//         document.write(i*6,"<br>")
+//     }
+// }
+// function table7(){
+//     for (i=1; i<=10; i++){
+//         document.write(i*7,"<br>")
+//     }
+// }
+// function table8(){
+//     for (i=1; i<=10; i++){
+//         document.write(i*8,"<br>")
+//     }
+// }
+// function table9(){
+//     for (i=1; i<=10; i++){
+//         document.write(i*9,"<br>")
+//     }
+// }
+// function table10(){
+//     for (i=1; i<=10; i++){
+//         document.write(i*10,"<br>")
+//     }
+// }
+// if (table==2){
+//     table2()
+// }
+// else if (table==3){
+//     table3()
+// }
+// else if (table==4){
+//     table4()
+// }
+// else if (table==5){
+//     table5()
+// }
+// else if (table==6){
+//     table6()
+// }
+// else if (table==7){
+//     table7()
+// }
+// else if (table==8){
+//     table8()
+// }
+// else if (table==9){
+//     table9()
+// }
+// else if (table==10){
+//     table10()
+// }
+
+
+
+
+// Chapter No 17 To 20 Question No 6
+// for (i=0; i<=10; i++){
+//     document.write(i)
+// }
+
+
+
+// Chapter No 17 To 20 Question No 7
+// var search_product = prompt("What You Want To Buy")
+// var  backery = ["apple", "mango", "banana", "gava"]
+// if (search_product=== backery){
+//     alert(search_product +"Is Avilable")
+// }
+// else{
+//     alert(search_product +"Is Not Avilable")
+// }
+
+
+
+
